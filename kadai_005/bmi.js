ecf3eb8f8981ec5b97d9991282423fcdc35b32b5
h3m9design <hino.m.design@gmail.com>
2024-11-03 00:07:21 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 体重
+let bodyWeight = 68;
+
+// 身長
+let height =1.7;
+
+// BMI
+let bmi =bodyWeight / (height * height);
+
+// 変数の値の確認
+console.log(bodyWeight);
+console.log(height);
+console.log(bmi);
